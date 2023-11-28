a7acc526ad7ebfb59dfa0ae882d29d80a09aab9b
Alek Michelson <am7590@rit.edu>
2023-11-28 16:26:43 -0500

    finish

--- generate_lab3_data.py
+++ generate_lab3_data.py
@@ -1,37 +1,25 @@
 import wikipedia
 
+def get_wiki_content(topic, language):
+    wikipedia.set_lang(language)
+    wiki_page = wikipedia.page(topic)
+    return wiki_page.content.replace('==', '').split()
 
-def break_into_groups(input_file, output_file):
-    """
-    Reads a text file, breaks the text into groups of 15 words, 
-    and writes it into another file with a specified format.
-    """
+def break_into_groups(language, output_file, topic):
     try:
-
-        wiki_page = wikipedia.page("Capybara")   
-        print(wiki_page.content)
- 
-       # print(page['extract'])
-
-        # Read the input file
-        # with open(input_file, 'r') as file:
-        #     text = file.read()
-
-        # Split the text into words
-        words = wiki_page.content.split()
-
-        # Initialize a list to store the groups
+        words = get_wiki_content(topic, language)
         groups = []
 
         # Iterate over the words and create 15-word groups
         for i in range(0, len(words), 15):
             group = ' '.join(words[i:i + 15])
-            groups.append(f"en|{group}")
+            groups.append(f"{language}|{group}")
 
-        # Write the groups to the output file
+        # Write to output file
         with open(output_file, 'w') as file:
             for group in groups:
-                file.write(group + '\n')
+                if len(group.split()) == 15:
+                    file.write(group + '\n')
 
         return True
 
@@ -39,8 +27,13 @@ def break_into_groups(input_file, output_file):
         print(f"An error occurred: {e}")
         return False
 
-# Example usage
-input_file_path = 'input.txt'  # Replace with your actual file path
-output_file_path = 'output.txt'  # Replace with your desired output file path
 
-break_into_groups(input_file_path, output_file_path)
+if __name__ == '__main__':
+    # Example usage
+    # input_file_path = 'input.txt'  # Replace with your actual file path
+    # output_file_path = 'output.txt'  # Replace with your desired output file path
+    language = input("Enter your language (en/nl): ").strip().lower()
+    output_file = input("Enter the path to your output file: ")
+    topic = input("Enter wikipedia a topic(s) to search for: ")
+
+    break_into_groups(language, output_file, topic)
